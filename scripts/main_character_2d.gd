539c62e836fe4dd5c84cc106edef81dc0030e01d
Ngoc Bao Tram Nguyen <ngocbaotramnguyen@bchamm.local>
2025-04-22 02:53:25 -0400

    Force merge: override main copy with feature branch

--- scripts/main_character_2d.gd
+++ scripts/main_character_2d.gd
@@ -1,79 +1,47 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-
-const JUMP_VELOCITY = -400.0
+const SPEED = 300.0
 #We command then move goose_test to the script 
 @onready var sprite_2d = $goose_test
+var last_direction_x = 1 #facing right
 
-#func get_input():
-	#var input_direction = Input.get_vector("left", "right", "up", "down")
-	#velocity = input_direction * SPEED
-	#prite_2d.animation = "walk"
 func _ready() -> void:
 	add_to_group("mainCharacter")
 	print("Player in groups:", get_groups()) 
 	
 func _physics_process(delta: float) -> void:
 	#Animations:
-	
-	if Input.is_action_pressed("ui_accept") and (not is_on_floor()):
-		velocity += get_gravity() * delta
-		sprite_2d.play("fly")
-
-	# Handle jump.
-	if Input.is_action_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		print("jump was just pressed")
-		sprite_2d.play("fly")
 
 	var input_direction = Vector2.ZERO # Initialize to zero
-	if is_on_floor():
-		if Input.is_action_pressed("right"):
+
+	if Input.is_action_pressed("right"):
 			input_direction.x = 1
-			velocity = input_direction * SPEED
-			sprite_2d.play("walk")
 		
-		if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left"):
 			input_direction.x = -1
-			velocity = input_direction * SPEED
-			sprite_2d.play("walk")
 		
-		if Input.is_action_pressed("up"):
+	if Input.is_action_pressed("up"):
 			input_direction.y = -1
-			velocity = input_direction * SPEED
-			sprite_2d.play("walk")
 	
-		if Input.is_action_pressed("down"):
+	if Input.is_action_pressed("down"):
 			input_direction.y = 1
-			velocity = input_direction * SPEED
-			sprite_2d.play("walk")
-	else:
-		if Input.is_action_pressed("right"):
-			input_direction.x = 1
-			velocity = input_direction * SPEED
-			sprite_2d.play("walk")
-		
-		if Input.is_action_pressed("left"):
-			input_direction.x = -1
-			velocity = input_direction * SPEED
-			sprite_2d.play("walk")
-		
-		if Input.is_action_pressed("up"):
-			input_direction.y = -1
-			velocity =input_direction  * SPEED
-			sprite_2d.play("walk")
-	
-		if Input.is_action_pressed("down"):
-			input_direction.y = 1
-			velocity = input_direction * SPEED
-			sprite_2d.play("walk")
+
+	input_direction = input_direction.normalized()
+	velocity = input_direction * SPEED
 	
 	move_and_slide()
-	
-	#velocity = input_direction * SPEED 
-	#=> Cannot put the velocity here because it would affect 
-	#vertical velocity (for jumping) bc it is overwritten 
-	
-	var isLeft = velocity.x<0
-	sprite_2d.flip_h = isLeft
+
+	#Animation
+	if input_direction != Vector2.ZERO:
+		sprite_2d.play("walk")
+	else:
+		sprite_2d.play("default")
+		
+	#Remember the x-value of direction when Jorge moves
+	if input_direction.x != 0:
+		last_direction_x = input_direction.x
+		
+	#Flip Jorge based on its last remembered direction: LEFT
+	sprite_2d.flip_h = last_direction_x < 0
+		
+		
