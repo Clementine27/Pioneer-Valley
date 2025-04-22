69f2e83db1eedd58cd96578a42f411ca18778e0e
Thu Ngo <160973994+Clementine27@users.noreply.github.com>
2025-04-22 00:51:43 -0400

    Revert "fix player movement"
    This reverts commit a61868829108eddec616f4716e104eb96574856e.

--- scripts/collision_hint.gd
+++ scripts/collision_hint.gd
@@ -4,8 +4,22 @@ extends StaticBody2D
 @onready var detection_area = $Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	## Ensure detection_area is valid
+	#if detection_area:
+		#print("detection_area is valid")
+	#else:
+		#print("detection_area is not assigned!")
+	
+	#Connect the area_entered signal from Area2D to this script's function
 	text_box.hide() #hide box initially 
 	
+	# Correctly connect the signal
+	#if detection_area.connect("area_entered", Callable(self, "_on_area_2d_area_entered")):
+		#print("Sucessfully")
+	#else:
+		#print("Failed to connect signal")
+		
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
@@ -19,8 +33,7 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		text_box.show()
 	else:
 		print("It is not in group 'mainCharacter'")
-	
-	
+		
 
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
